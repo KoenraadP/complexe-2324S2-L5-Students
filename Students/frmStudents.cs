ac8697cd66bce2b97c094cadd33c9f75df57bed2
Koenraad Pecceu <koenraad.pecceu@gmail.com>
2024-03-14 14:58:20 +0100

    Add finished solution

--- Students/frmStudents.cs
+++ Students/frmStudents.cs
@@ -15,6 +15,27 @@ namespace Students
         public frmStudents()
         {
             InitializeComponent();
+            LoadStudents();
+        }
+
+        private void LoadStudents()
+        {
+            Student student1 = new Student(1, "Tess", "Tickles", new DateTime(1985, 10, 30), "Programmeren");
+            Student student2 = new Student(2, "Hugh", "Jass", new DateTime(1990, 1, 25), "Programmeren");
+            Student student3 = new Student(3, "Mike", "Rotch", new DateTime(1995, 5, 6), "Besturingssystemen");
+
+            cbxStudents.Items.Add(student1);
+            cbxStudents.Items.Add(student2);
+            cbxStudents.Items.Add(student3);
+        }
+
+        private void cbxStudents_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Student student = (Student)cbxStudents.SelectedItem;
+
+            txtName.Text = student.LastName + " " + student.FirstName;
+            txtAge.Text = student.CalculateAge().ToString();
+            txtCourse.Text = student.Course;
         }
     }
 }
